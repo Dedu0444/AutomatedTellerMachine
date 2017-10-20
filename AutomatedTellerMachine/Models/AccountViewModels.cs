93a843ac703418a0453d773cce98da67563a7521
Amy Burayu <dedu0444@gmail.com>
2017-10-20 17:21:52 -0400

    ModelsandData -Generating DB and creating views and Models

--- AutomatedTellerMachine/Models/AccountViewModels.cs
+++ AutomatedTellerMachine/Models/AccountViewModels.cs
@@ -64,11 +64,20 @@ namespace AutomatedTellerMachine.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
+   
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
